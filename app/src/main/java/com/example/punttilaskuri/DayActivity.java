64590411aa7252af8fdcbdd98e4957d92aa683aa
Mikhail Deriabin <mikko.deriabin@gmail.com>
2021-04-18 20:38:22 +0300

    1. JsonFileHandler created: reads .json files 2. daysInfo.json created: contains information about user's plans like trainings and notes

--- app/src/main/java/com/example/punttilaskuri/DayActivity.java
+++ app/src/main/java/com/example/punttilaskuri/DayActivity.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class DayActivity extends AppCompatActivity {
 
     // UI elements
     private TextView dateTV, jsonFileTV;
-    private Button loadJsonButton;
+    private EditText noteInputField;
+    private Button loadJsonButton, addNoteButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,16 +29,29 @@ public class DayActivity extends AppCompatActivity {
         int choseMonth = b.getInt("choseMonth", 1);
         int choseDay = b.getInt("choseDay", 1);
 
+        //Defining UI elements
         dateTV = findViewById(R.id.dateTV);
         jsonFileTV = findViewById(R.id.jsonFileTV);
         loadJsonButton = findViewById(R.id.loadJsonButton);
+        noteInputField = findViewById(R.id.noteInputField);
+        addNoteButton = findViewById(R.id.addNoteButton);
 
         String choseDate = choseDay + "." + choseMonth + "." + choseYear;
         dateTV.setText(choseDate);
 
+        //Events
         loadJsonButton.setOnClickListener( v -> {
-            File daysInfoFile = new File("");
-            //JSONObject allDayInformation =
+            JsonFileHandler jsonFileHandler = new JsonFileHandler("daysInfo.json", this);
+            try {
+                String result = jsonFileHandler.getJsonObject().getJSONObject("days").getJSONObject("1.2.2021").getJSONArray("notes").getString(0);
+                jsonFileTV.setText(result);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        addNoteButton.setOnClickListener( v -> {
+
         });
     }
 }
---
+++ app/src/main/java/com/example/punttilaskuri/UserInputHandler.java
@@ -0,0 +1,11 @@
+package com.example.punttilaskuri;
+
+import org.json.JSONObject;
+
+public class UserInputHandler {
+    JSONObject jsonObject;
+
+    public void addNote(String note){
+        //jsonObject = new JsonFileHandler("daysInfo.json");
+    }
+}
