cc4a2d4f4c400a769f561e6bb4bb18a55f38a112
samimie <sami_miettinen@hotmail.fi>
2021-04-20 11:51:26 +0300

    Added class InfoDump for testing stuff

---
+++ app/src/main/java/com/example/punttilaskuri/InfoDump.java
@@ -0,0 +1,12 @@
+package com.example.punttilaskuri;
+
+import java.util.ArrayList;
+
+public class InfoDump {
+
+    public ArrayList<String> noteList;
+
+    public InfoDump(){
+        this.noteList = new ArrayList<String>();
+    }
+}
