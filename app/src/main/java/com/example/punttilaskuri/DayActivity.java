95bf68dc71a76d2710d109edd82a3e59f5616419
samimie <sami_miettinen@hotmail.fi>
2021-05-03 18:19:59 +0300

    Notes as listview

--- app/src/main/java/com/example/punttilaskuri/DayActivity.java
+++ app/src/main/java/com/example/punttilaskuri/DayActivity.java
@@ -2,23 +2,43 @@ package com.example.punttilaskuri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.punttilaskuri.fileHandlers.NotesHandler;
 
 import org.json.JSONException;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class DayActivity extends AppCompatActivity {
 
     // UI elements
-    private TextView dateTV, jsonFileTV;
+    private TextView dateTV;
     private EditText noteInputField;
+    private ListView noteView;
     private Button addNoteButton, clearAllNotesButton;
+    // ListView Elements
+    private NoteAdapter noteAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +51,6 @@ public class DayActivity extends AppCompatActivity {
 
         //Defining UI elements
         dateTV = findViewById(R.id.dateTV);
-        jsonFileTV = findViewById(R.id.jsonFileTV);
         noteInputField = findViewById(R.id.noteInputField);
         addNoteButton = findViewById(R.id.addNoteButton);
         clearAllNotesButton = findViewById(R.id.clearAllNotesButton);
@@ -40,11 +59,15 @@ public class DayActivity extends AppCompatActivity {
         dateTV.setText(choseDate);
 
         NotesHandler notesHandler = new NotesHandler(this);
-        String userNotes = null;
-        userNotes = notesHandler.getDayInformationAsString(choseDate);
-        jsonFileTV.setText(userNotes);
+
+        // ListView stuff
+        noteView = (ListView) findViewById(R.id.noteView);
+        ArrayList<String> notes = new ArrayList<>();
+        noteAdapter = new NoteAdapter(this, android.R.layout.simple_list_item_1, notes);
+        noteView.setAdapter(noteAdapter);
 
         //Events
+
         addNoteButton.setOnClickListener( v -> {
             String userInput = noteInputField.getText().toString();
             try {
@@ -53,20 +76,52 @@ public class DayActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-            String allNotes = null;
-            allNotes = notesHandler.getDayInformationAsString(choseDate);
-            jsonFileTV.setText(allNotes);
-        });
+            String allNotes = notesHandler.getDayInformationAsString(choseDate);
+            String[] lines = allNotes.split("\n");
+            notes.clear();
+            notes.addAll(Arrays.asList(lines));
+            noteAdapter = new NoteAdapter(this, android.R.layout.simple_list_item_1, notes);
+            noteView.setAdapter(noteAdapter);
 
-        jsonFileTV.setOnClickListener( v -> {
-            notesHandler.removeOneItem(choseDate, 0);
-            String allNotes = null;
-            allNotes = notesHandler.getDayInformationAsString(choseDate);
-            jsonFileTV.setText(allNotes);
         });
 
         clearAllNotesButton.setOnClickListener( v -> {
             notesHandler.clearWholeJsonFile("daysInfo.json");
+            notes.clear();
+            noteAdapter = new NoteAdapter(this, android.R.layout.simple_list_item_1, notes);
+            noteView.setAdapter(noteAdapter);
         });
+
+        //Listview onClickListener
+        noteView.setOnItemClickListener((parent, view, position, id) -> Toast.makeText(getApplicationContext(),
+                "Click ListItem Number " + position, Toast.LENGTH_LONG)
+                .show());
+    }
+    private class NoteAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> noteMap = new HashMap<>();
+
+        public NoteAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                noteMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return noteMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+    }
+    public void updateNoteList(String noteLines){
+
     }
 }
+
